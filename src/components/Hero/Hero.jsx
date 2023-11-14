733ce7be79182bfb497a0fa6832060065abd8df2
ThomasWarin <thomas.warin.dev+github@gmail.com>
2023-11-14 16:29:47 +0100

    [Front] Ajout de la possibilité d'afficher les attaques en responsive mobile

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/no-unescaped-entities */
 
 // Import du style
@@ -8,6 +11,7 @@ import { useMediaQuery } from 'react-responsive';
 
 // Import React
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Hero({
   background,
@@ -23,7 +27,8 @@ function Hero({
 }) {
   const max1000px = useMediaQuery({ query: '(max-width: 1000px)' });
   const max750px = useMediaQuery({ query: '(max-width: 750px)' });
-  const max500px = useMediaQuery({ query: '(max-width: 500px)' });
+
+  const [blockVisible, setBlockVisible] = useState('description');
 
   return (
     <main className="Hero" style={{ backgroundImage: `url(${background})` }}>
@@ -32,22 +37,53 @@ function Hero({
           <h2 className="Pokemon-name">{name}</h2>
           <img className="Pokemon-image" src={pokemon} alt={name} />
         </div>
+
         {!max750px && <div className="Hero-section-separator" />}
+
         <div className="Hero-section-right">
-          <h3 className="Pokemon-section">DESCRIPTION</h3>
-          <p>{description}</p>
+          <h3
+            // prettier-ignore
+            className={`Pokemon-section ${ blockVisible === 'description' ? 'active' : '' }`}
+            onClick={() => setBlockVisible('description')}
+          >
+            DESCRIPTION
+          </h3>
+          <p
+            // prettier-ignore
+            className={`Pokemon-description ${blockVisible  === 'description' ? 'active' : 'inactive'}`}
+          >
+            {description}
+          </p>
+
           {!max1000px && <div className="Hero-section-line" />}
+
           <h3 className="Pokemon-section">ATTAQUES</h3>
-          <div className="Pokemon-attack">
+          <div
+            className="Pokemon-attack"
+            onClick={() => setBlockVisible('attack1')}
+          >
             <h4>{attack1}</h4>
             {attack1PV !== 0 && <span>{attack1PV}</span>}
           </div>
-          {!max500px && <p>{attack1Description}</p>}
-          <div className="Pokemon-attack">
+          <p
+            // prettier-ignore
+            className={`Pokemon-attack-1 ${blockVisible  === 'attack1' ? 'active' : 'inactive'}`}
+          >
+            {attack1Description}
+          </p>
+          <div
+            className="Pokemon-attack"
+            onClick={() => setBlockVisible('attack2')}
+          >
             <h4>{attack2}</h4>
             {attack2PV !== 0 && <span>{attack2PV}</span>}
           </div>
-          {!max500px && <p>{attack2Description}</p>}
+          <p
+            // prettier-ignore
+            className={`Pokemon-attack-2 ${blockVisible  === 'attack2' ? 'active' : 'inactive'}`}
+          >
+            {attack2Description}
+          </p>
         </div>
       </section>
     </main>
